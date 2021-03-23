2937cdfe77a09dc40345f0e679090e312ab1fc64
sans1110 <sanskrutinakhale99@gmail.com>
2021-03-23 05:48:10 +0530

    Update about.dart

--- about.dart
+++ about.dart
@@ -1,38 +1,55 @@
 import "package:flutter/material.dart";
 
 import 'package:getwidget/getwidget.dart';
+
 void main()
 {
   runApp(
     MaterialApp(
-      home: aboutus()
+      home: about()
     )
   );
 }
 
-class aboutus extends StatelessWidget{
+class about extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white30,
+
       appBar: AppBar(title: Text('ABOUT PAGE'),),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Image.asset('assets/images/VNIT_Nagpur_main.jpg'),
-            GFAccordion(
-              title: "ABOUT APP",
-              content: "VNIT recently introduced a shuttle service on campus which made conveyance extremely convenient. The shuttles follow pre-defined routes around the campus, but the timing of their arrival and departure from a stop is generally unknown.Our project, Racket-Shuttle Tracker, aims to work around these issues as well as make it all-the-more comfortable for the people on campus to use.",
-            ),GFAccordion(
-              title: "ABOUT US",
-              content: "VNIT",
-            ),GFAccordion(
-              title: "CONTACT US",
-              content: "Email id: xxyz@gmail.com "
-                  "     Phone Number: 8769543231"
-              "         Address : VNIT S.Ambazari Road, Nagpur",
-            )
-          ],
-        )
+      body:SingleChildScrollView(
+
+          child: Container(
+          child: Column(
+            children: <Widget>[
+
+              Image.asset('assets/images/VNIT_Nagpur_main.jpg'),
+
+              GFAccordion(
+                title: "ABOUT APP",textStyle: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold, color: Colors.black54),
+                content: "\n VNIT recently introduced a shuttle service on campus which made conveyance extremely convenient."
+                "The shuttles follow pre-defined routes around the campus, but the timing of their arrival and departure from a stop is generally unknown.\n\nOur project, Racket-Shuttle Tracker, aims to work around these issues as well as make it all-the-more comfortable for the people on campus to use.\n",
+                collapsedIcon: Icon(Icons.add_circle),
+                expandedIcon: Icon(Icons.arrow_circle_up_rounded),
+
+              ),GFAccordion(
+                title: "ABOUT US",
+                content: "VNIT \n(need to be updated)", textStyle: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.black54),
+                collapsedIcon: Icon(Icons.add_circle),
+                expandedIcon: Icon(Icons.arrow_circle_up_rounded),
+
+              ),GFAccordion(
+                title: "CONTACT US",
+                content: "Email id: xxyz@gmail.com \n \n Phone Number: 8769543231 \n \n Address : VNIT S.Ambazari Road, Nagpur",textStyle: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold, color: Colors.black54),
+                collapsedIcon: Icon(Icons.add_circle),
+                expandedIcon: Icon(Icons.arrow_circle_up_rounded),
+
+              )
+            ],
+          )
+        ),
       ),
     );
   }
